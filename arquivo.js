5fb9e34b95f7d7b852ed1c2ce5cb94d6a5162f06
Paulo Camargo <fernandesmetal721@gmail.com>
2025-03-08 19:20:48 -0300

    Modifica arquivo.js para adicionar funcionalidade X

--- arquivo.js
+++ arquivo.js
@@ -12,7 +12,7 @@ function exibirConteudo(conteudo) {
     // Configura um timeout para esconder o conteúdo após 5 segundos (5000 ms)
     setTimeout(function () {
         conteudoDinamico.style.display = 'none'; // Esconde a div
-    }, 22000); // 22000 milissegundos = 22 segundos
+    }, 23000); // 23000 milissegundos = 23 segundos
 }
 
 // Evento para o botão "Habilidades"
